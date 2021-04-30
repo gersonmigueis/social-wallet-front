248dc7b081de475eec212d0fc6496686daa2fb6b
Gerson <gersonmigueis10@gmail.com>
2021-04-30 02:22:47 -0400

    [ADD] - Padronizando Fontes

---
+++ src/styles/styles.js
@@ -0,0 +1,13 @@
+//Tipografia de fontes
+export const FONT_FAMILY_LIGHT = 'OpenSans-Light';
+export const FONT_FAMILY_REGULAR = 'OpenSans-Regular';
+export const FONT_FAMILY_SEMI_BOLD = 'OpenSans-SemiBold';
+export const FONT_FAMILY_BOLD = 'OpenSans-Bold';
+export const FONT_FAMILY_EXTRA_BOLD = 'OpenSans-ExtraBold';
+//medidas
+export const FONT_SIZE_XX_LARGE = 30;
+export const FONT_SIZE_X_LARGE = 24;
+export const FONT_SIZE_LARGE = 20;
+export const FONT_SIZE_MEDIUM = 16;
+export const FONT_SIZE_SMALL = 14;
+export const FONT_SIZE_X_SMALL = 12;
